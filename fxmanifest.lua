03feb37d6ede1b2c7a20984d1493b90ce55af305
Sinyx <sinyx.contact@gmail.com>
2021-12-20 14:07:26 +0100

    Clean repository

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ version '1.0'
 
 server_only 'yes'
 
-debug_log 'yes' -- set this to 'no' to disable output log messages
+debug_log 'no' -- set this to 'yes' to enable output log messages
 debug_message '[INSTANCE]: Player %s entered instance %s'
 
 server_scripts {
